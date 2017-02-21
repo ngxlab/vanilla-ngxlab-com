cdcf42c163f7999ecb2d18178a03305da73b7386
周晶 <zhoujing_k49@163.com>
2017-02-22 00:14:21 +0800

    vanilla.ngxlab.com start

---
+++ spec/spec_helper.lua
@@ -0,0 +1,16 @@
+package.path = package.path .. ";/?.lua;/?/init.lua;/media/psf/g/idevz/code/www/vanilla/framework/0_1_0_rc7/?.lua;/media/psf/g/idevz/code/www/vanilla/framework/0_1_0_rc7/?/init.lua;;";
+package.cpath = package.cpath .. ";/?.so;/media/psf/g/idevz/code/www/vanilla/framework/0_1_0_rc7/?.so;;";
+
+Registry={}
+Registry['APP_ROOT'] = '/media/psf/g/idevz/code/z/git/vanilla.ngxlab.com'
+Registry['APP_NAME'] = 'vanilla.ngxlab.com'
+
+LoadV = function ( ... )
+    return require(...)
+end
+
+LoadApp = function ( ... )
+    return require(Registry['APP_ROOT'] .. '/' .. ...)
+end
+
+LoadV 'vanilla.spec.runner'
